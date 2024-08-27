7af3e7445e3fb901dc611769bd1f28db914597c4
Manuel <ferrarismanu@gmail.com>
2024-08-27 19:30:51 -0300

    Creacion de Carpetas para CRUDs Frontend

--- CRUDFrontend/client/src/pages/peluquero/Peluqueropage.js
+++ CRUDFrontend/client/src/pages/peluquero/Peluqueropage.js
@@ -13,7 +13,6 @@ const PeluqueroPage = () => {
 
     const fetchPeluqueros = async () => {
         const data = await getAllPeluqueros();
-        console.log(data);
         setPeluqueros(data);
     };
 
