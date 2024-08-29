d68f3a52351c330f162462cbecf33564c76e95dc
Manuel <ferrarismanu@gmail.com>
2024-08-29 20:27:17 -0300

    Se agragan validaciones

--- CRUDFrontend/client/src/pages/peluquero/Peluqueropage.js
+++ CRUDFrontend/client/src/pages/peluquero/Peluqueropage.js
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Axios from 'axios';
+import React, { useEffect, useState } from 'react'; //Hook que permite hacer efectos secundarios en componentes funcionales (lo usamos para carga y actualizacion de datos).
+                                                    //Hook que permite gestionar el estado local dentro de un componente funcional.
+import 'bootstrap/dist/css/bootstrap.min.css';      //Importa el archovo CDD de bootstrap, nosotros usamos.
+import Axios from 'axios'; //Biblioteca para hacer peticiones HTTP, (conecta el Front con el Back)
+//FALTAN IMPORTACIONES DEL SERVIE PARA MODULARIZAR
 
-function PeluqueroList() {
+function PeluqueroList() { //Maneja la logica y renderizacion de la lista peluqueros
     const [peluqueros, setPeluqueros] = useState([]); // Lista de Peluqueros
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [errors, setErrors] = useState({});
-    const [nombre, setNombre] = useState('');
+    const [loading, setLoading] = useState(true); //Estado que indica que los datos se estan cargando
+    const [error, setError] = useState(null); //Cualquier error que ocurra durante la carga de datos.
+    const [errors, setErrors] = useState({}); //Errores de validacion del formulario.
+    const [nombre, setNombre] = useState(''); 
     const [fecha_Ingreso, setFechaIngreso] = useState('');
     const [tipo, setTipo] = useState('');
-    const [editar, setEditar] = useState(false);
-    const [peluqueroSeleccionado, setPeluqueroSeleccionado] = useState(null);
+    const [editar, setEditar] = useState(false); //Indica si esta en modo de edicion
+    const [peluqueroSeleccionado, setPeluqueroSeleccionado] = useState(null); //Indica el pekuquero que se selecciono
 
-    useEffect(() => {
+    useEffect(() => { //Se ejecuta cuando el componente se monta por primera vez, hace una peticion a la API y obtiene la lista de peluqueros
         const fetchPeluqueros = async () => {
             try {
                 const response = await fetch('http://localhost:3000/api/peluqueros');
@@ -21,7 +23,7 @@ function PeluqueroList() {
                     throw new Error('Network response was not ok');
                 }
                 const data = await response.json();
-                setPeluqueros(data.data || []); // Asegúrate de que esta propiedad exista en la respuesta JSON
+                setPeluqueros(data.data || []);
             } catch (error) {
                 setError(error.message);
             } finally {
@@ -31,15 +33,15 @@ function PeluqueroList() {
         fetchPeluqueros();
     }, []);
 
-    useEffect(() => {
+    useEffect(() => { //Actualiza los campos del peluquero seleccionado
         if (peluqueroSeleccionado) {
             setNombre(peluqueroSeleccionado.nombre || '');
             setFechaIngreso(peluqueroSeleccionado.fecha_Ingreso || '');
             setTipo(peluqueroSeleccionado.tipo || '');
         }
     }, [peluqueroSeleccionado]);
-
-    const getPeluqueros = async () => {
+ 
+    const getPeluqueros = async () => { //Hace la peticion GET a la API
         try {
             const response = await Axios.get('http://localhost:3000/api/peluqueros');
             const peluqueros = response.data.data;
@@ -52,7 +54,7 @@ function PeluqueroList() {
         }
     };
 
-    const validateForm = () => {
+    const validateForm = () => { //Valida los datos ingresados al formulario
         const errors = {};
         const today = new Date().toISOString().split('T')[0];
 
@@ -64,8 +66,8 @@ function PeluqueroList() {
 
         if (!fecha_Ingreso) {
             errors.fecha_Ingreso = "La fecha de ingreso es obligatoria.";
-        } else if (fecha_Ingreso >= today) {
-            errors.fecha_Ingreso = "La fecha de ingreso no puede ser mayor o igual a la fecha actual.";
+        } else if (fecha_Ingreso > today) {
+            errors.fecha_Ingreso = "La fecha de ingreso no puede ser mayor a la fecha actual.";
         }
 
         if (!tipo) {
@@ -77,7 +79,7 @@ function PeluqueroList() {
         return errors;
     };
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e) => { //Maneja el envio del formulario, validando los datos y enviandolos a la API (Para el PUT o POST)
         e.preventDefault();
         const validationErrors = validateForm();
         if (Object.keys(validationErrors).length > 0) {
@@ -114,29 +116,38 @@ function PeluqueroList() {
     };
 
     const eliminarPeluquero = (codigo_peluquero) => {
-        console.log('Eliminando peluquero con código:', codigo_peluquero);
-        Axios.delete(`http://localhost:3000/api/peluqueros/${codigo_peluquero}`)
-            .then(() => {
-                getPeluqueros();
-                alert('Peluquero Eliminado');
-            })
-            .catch(error => {
-                console.error('Error al eliminar el peluquero:', error);
-                alert('Error al eliminar el peluquero');
-            });
+        const confirmar = window.confirm(`¿Estás seguro de que deseas eliminar el peluquero con código ${codigo_peluquero}?`);
+        if (confirmar) {
+            console.log('Eliminando peluquero con código:', codigo_peluquero);
+            Axios.delete(`http://localhost:3000/api/peluqueros/${codigo_peluquero}`)
+                .then(() => {
+                    getPeluqueros();
+                    alert('Peluquero Eliminado');
+                })
+                .catch(error => {
+                    console.error('Error al eliminar el peluquero:', error);
+                    alert('Error al eliminar el peluquero');
+                });
+        } else {
+            console.log('Eliminación cancelada por el usuario.');
+        }
     };
 
+    //Renderizado condicional
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error}</div>;
 
     return (
         <div className="container">
             <div className="card">
+                
                 <div className="card-header">
                     GESTION DE PELUQUEROS
                 </div>
+
                 <div className="card-body">
                     <form onSubmit={handleSubmit}>
+
                         <div className="input-group mb-3">
                             <span className="input-group-text" id="basic-addon1">Nombre y Apellido:</span>
                             <input type="text"
@@ -144,6 +155,7 @@ function PeluqueroList() {
                                 className="form-control" value={nombre || ""} placeholder="Nombre y Apellido" aria-label="Nombre y Apellido" aria-describedby="basic-addon1" />
                             {errors.nombre && <div className="text-danger">{errors.nombre}</div>}
                         </div>
+
                         <div className="input-group mb-3">
                             <span className="input-group-text" id="basic-addon1">Fecha de Ingreso:</span>
                             <input type="date"
@@ -151,6 +163,7 @@ function PeluqueroList() {
                                 className="form-control" value={fecha_Ingreso || ""} aria-label="Fecha de Ingreso" aria-describedby="basic-addon1" />
                             {errors.fecha_Ingreso && <div className="text-danger">{errors.fecha_Ingreso}</div>}
                         </div>
+
                         <div className="input-group mb-3">
                             <span className="input-group-text" id="basic-addon1">Tipo:</span>
                             <select
@@ -176,12 +189,14 @@ function PeluqueroList() {
                                     <button type="submit" className='btn btn-success'>Registrar</button>
                             }
                         </div>
+
                     </form>
                 </div>
                 <div className="card-footer text-muted"></div>
             </div>
 
             <table className="table table-striped mt-3">
+
                 <thead>
                     <tr>
                         <th scope="col">Codigo</th>
@@ -191,6 +206,7 @@ function PeluqueroList() {
                         <th scope="col">ACCIONES</th>
                     </tr>
                 </thead>
+
                 <tbody>
                     {
                         peluqueros.map(val => (
@@ -209,6 +225,7 @@ function PeluqueroList() {
                         ))
                     }
                 </tbody>
+
             </table>
         </div>
     );
