801918c9a15c1645e7caa933f1a8fdcb7cf626be
Manuel <ferrarismanu@gmail.com>
2024-10-04 09:32:50 -0300

    Se agrega CRUD de Turno en Frontend

--- CRUDFrontend/client/src/App.js
+++ CRUDFrontend/client/src/App.js
@@ -4,6 +4,7 @@ import PeluqueroPage from "./pages/peluquero/Peluqueropage.js";
 import LocalidadesPage from "./pages/localidad/Localidad.page.js";
 import TipoServicioPage from "./pages/tipoServicio/Tiposervicio.page.js";
 import ServiciosPage from "./pages/servicio/Servicio.page.js";
+import TurnosPage from "./pages/turno/Turno.page.js";
 import MenuPage from './pages/MenuPage';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/localidad" element={<LocalidadesPage />} />
                 <Route path="/tiposervicio" element={<TipoServicioPage />} />
                 <Route path= "/servicio" element={<ServiciosPage/>} />
+                <Route path= "/turno" element={<TurnosPage/>} />
 
             </Routes>
         </Router>
