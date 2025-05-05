359b510f3ca28eab1035fd5f9a63cc1d03787a49
Manuel <ferrarismanu@gmail.com>
2025-05-05 08:27:24 -0300

    Se agraga acceso y actualizacion por rol

--- CRUDBackend/src/peluquero/peluquero.routes.ts
+++ CRUDBackend/src/peluquero/peluquero.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { findAll, getOne, add, update, remove, sanitizePeluqueroInput } from "./peluquero.controler.js";
 import { authMiddleware } from "../auth/auth.middleware.js";
+import { authorizeRole } from "../auth/authorizeRole.js";
+
 export const peluqueroRouter = Router()
 
 //peluqueroRouter.get('/', findAll) <-esta ruta no es segura, no requiere autenticación
-peluqueroRouter.get('/', authMiddleware, findAll) // Ruta protegida
+peluqueroRouter.get('/', authMiddleware, authorizeRole('peluquero'), findAll) // Ruta protegida
 peluqueroRouter.get('/:codigo_peluquero', getOne) //Aqui si indicamos el codigo para realizar la busqueda.
 peluqueroRouter.post('/', sanitizePeluqueroInput, add)
-peluqueroRouter.put('/:codigo_peluquero',sanitizePeluqueroInput, update)
+peluqueroRouter.put('/:codigo_peluquero', authorizeRole('peluquero'), sanitizePeluqueroInput, update)
 peluqueroRouter.delete('/:codigo_peluquero', remove)
