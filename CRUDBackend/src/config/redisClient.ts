3451e1a0d9c05cae9f02ef32f04995e1eefb6f63
Manuel <ferrarismanu@gmail.com>
2025-09-15 10:04:59 -0300

    Se ajusta redisClient

--- CRUDBackend/src/config/redisClient.ts
+++ CRUDBackend/src/config/redisClient.ts
@@ -4,6 +4,7 @@ const redisClient = new Redis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
     password: process.env.REDIS_PASSWORD,
+    tls: {}, // es opc. pero si Railway requiere conexión segura ya lo tiene.
 });
 
 export default redisClient;
