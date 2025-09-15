f9d0cdbf2706c391529e0fee2b957df02bd19b51
Manuel <ferrarismanu@gmail.com>
2025-09-15 12:12:30 -0300

    Se crea variable de entorno para Produccion

--- Frontend/client/src/auth/constants.ts
+++ Frontend/client/src/auth/constants.ts
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:3000/api"; // URL base de la API
+//export const API_URL = "http://localhost:3000/api"; // URL base de la API (Desarrollo)
+export const API_URL = process.env.REACT_APP_API_URL as string; // URL base de la API (Producción)
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const REFRESH_TOKEN_KEY = 'refreshToken';
 export const USER_DATA_KEY = 'user';
