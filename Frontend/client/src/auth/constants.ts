f53cf6bcaef30e311fcd57984d7adc6f83075a7e
Manuel <ferrarismanu@gmail.com>
2025-09-15 21:12:39 -0300

    Se concatena ruta del front

--- Frontend/client/src/auth/constants.ts
+++ Frontend/client/src/auth/constants.ts
@@ -1,5 +1,5 @@
 //export const API_URL = "http://localhost:3000/api"; // URL base de la API (Desarrollo)
-export const API_URL = process.env.REACT_APP_API_URL as string; // URL base de la API (Producción)
+export const API_URL = (process.env.REACT_APP_API_URL + "/api") as string; // URL base de la API (Producción)
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const REFRESH_TOKEN_KEY = 'refreshToken';
 export const USER_DATA_KEY = 'user';
